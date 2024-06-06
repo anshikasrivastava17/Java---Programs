cbfb5edcc0aa8942ef2e9b1442a448082181ce90
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2024-06-06 18:01:46 +0530

    selection sort

--- Sorting/Selection_Sort.java
+++ Sorting/Selection_Sort.java
@@ -6,7 +6,7 @@
             // Find the minimum element in unsorted array
             int p = i;
             for (int j = i+1; j < n; j++)
-                if (arr[j] < arr[min_idx])
+                if (arr[j] < arr[p])
                     p = j;
             int temp = arr[p];
             arr[p] = arr[i];
