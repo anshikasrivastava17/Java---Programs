c8524a862631d36fd2ca87a690d4f9dc1cc3b466
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2024-09-02 00:08:47 +0530

    sum of n numbers

---
+++ Recursion/Sum.java
@@ -0,0 +1,16 @@
+//sum of n numbers
+
+public class Sum {
+    public static void main(String[] args) {
+        int ans = sum(5);
+        System.out.println(ans);
+    }
+
+    static int sum(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        return n + sum(n-1);
+    }
+}
