c8cb3090e4f41500ba0dadfcc9074e5b46aec57b
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2024-09-04 01:14:31 +0530

    multiple occurrences linear search

---
+++ Recursion/Linear2.java
@@ -0,0 +1,12 @@
+//multiple occurences linear search
+//{2,3,1,4,4,5} and target = 4 , return all occurences
+ static ArrayList<Integer> list = new ArrayList<>();
+    static void findAllIndex(int[] arr, int target, int index) {
+        if (index == arr.length) {
+            return;
+        }
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        findAllIndex(arr, target, index + 1);
+    }
