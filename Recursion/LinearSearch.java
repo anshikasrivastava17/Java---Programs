808db1696e90c9a23683e95447da03ef361d01e6
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2024-09-04 01:04:10 +0530

    linear search

---
+++ Recursion/LinearSearch.java
@@ -0,0 +1,6 @@
+    static boolean find(int[] arr, int target, int index) {
+        if (index == arr.length) {
+            return false;
+        }
+        return arr[index] == target || find(arr, target, index + 1);
+    }
