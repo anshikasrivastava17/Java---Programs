2cdb2ce18572e73321af1f7415b832277e902c18
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2024-09-04 01:09:25 +0530

    linear search

--- Recursion/LinearSearch.java
+++ Recursion/LinearSearch.java
@@ -4,3 +4,27 @@
         }
         return arr[index] == target || find(arr, target, index + 1);
     }
+
+//returning index in linear search
+static int findIndex(int[] arr, int target, int index) {
+        if (index == arr.length) {
+            return -1;
+        }
+        if (arr[index] == target) {
+            return index;
+        } else {
+            return findIndex(arr, target, index + 1);
+        }
+    }
+
+//traversing array in reverse direction
+static int findIndexLast(int[] arr, int target, int index) {
+        if (index == -1) {
+            return -1;
+        }
+        if (arr[index] == target) {
+            return index;
+        } else {
+            return findIndexLast(arr, target, index - 1);
+        }
+    }
