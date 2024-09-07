9b3e8986e6788f97d1839e68994d8786511800c7
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2024-09-08 01:47:02 +0530

    return arrlist on linear search multiple occurences

---
+++ Recursion/ArrList.java
@@ -0,0 +1,32 @@
+//Linear search on multiple occurrences
+
+//M1
+static ArrayList<Integer> findAllIndex(int[] arr, int target, int index, ArrayList<Integer> list) {
+        if (index == arr.length) {
+            return list;
+        }
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        return findAllIndex(arr, target, index + 1, list);
+    }
+
+//M2
+    static ArrayList<Integer> findAllIndex2(int[] arr, int target, int index) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        if (index == arr.length) {
+            return list;
+        }
+
+        // this will contain answer for that function call only
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        ArrayList<Integer> ansFromBelowCalls = findAllIndex2(arr, target, index + 1);
+
+        list.addAll(ansFromBelowCalls);
+
+        return list;
+    }
