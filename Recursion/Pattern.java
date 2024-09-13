399b84c07e700f94f7b6cf9b0d61879bb69a9cd8
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2024-09-14 01:10:58 +0530

    triangle pattern

---
+++ Recursion/Pattern.java
@@ -0,0 +1,17 @@
+*
+* *
+* * *
+* * * *
+
+  static void triangle(int r, int c) {
+        if (r == 0) {
+            return;
+        }
+        if (c < r) {
+            System.out.print("*");
+            triangle(r, c+1);
+        } else {
+            System.out.println();
+            triangle(r-1, 0);
+        }
+    }
