1d93b873ae3fa13f6446e525e80f8c911d21e5bd
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2024-09-15 23:16:25 +0530

    count of possible permutations

---
+++ Recursion/PermutationCount.java
@@ -0,0 +1,15 @@
+//counting number of possible permutations by taking count in the func body. 
+//Using number of recursive calls to count instead of using n! formula
+static int permutationsCount(String p, String up) {
+        if (up.isEmpty()) {
+            return 1;
+        }
+        int count = 0;
+        char ch = up.charAt(0);
+        for (int i = 0; i <= p.length(); i++) {
+            String f = p.substring(0, i);
+            String s = p.substring(i, p.length());
+            count = count + permutationsCount(f + ch + s, up.substring(1));
+        }
+        return count;
+    }
