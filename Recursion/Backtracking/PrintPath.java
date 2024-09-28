334b328022dfbe38782fd5e674ad30cea8e49df2
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2024-09-29 01:27:33 +0530

    printing paths from one point to other in a maze

---
+++ Recursion/Backtracking/PrintPath.java
@@ -0,0 +1,36 @@
+// printing actual paths in maze from one point to another
+static void path(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            System.out.println(p);
+            return;
+        }
+
+        if (r > 1) {
+            path(p + 'D', r-1, c);
+        }
+
+        if (c > 1) {
+            path(p + 'R', r, c-1);
+        }
+    }
+
+//storing paths in ArrayList
+ static ArrayList<String> pathRet(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+
+        if (r > 1) {
+            list.addAll(pathRet(p + 'D', r-1, c));
+        }
+
+        if (c > 1) {
+            list.addAll(pathRet(p + 'R', r, c-1));
+        }
+
+        return list;
+ }
