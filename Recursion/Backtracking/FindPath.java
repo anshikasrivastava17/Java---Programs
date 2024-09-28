f303b16941c0a1d41fc324f890f7b760af667feb
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2024-09-29 01:25:03 +0530

    find number of paths in maze

---
+++ Recursion/Backtracking/FindPath.java
@@ -0,0 +1,9 @@
+//find number of paths in the given maze from one point to other
+static int count(int r, int c) {
+        if (r == 1 || c == 1) {
+            return 1;
+        }
+        int left = count(r-1, c);
+        int right = count(r, c-1);
+        return left + right;
+    }
