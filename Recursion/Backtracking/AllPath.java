53757b9f3cc86500e8c26269a1e1c9d8ad51a1f9
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2024-10-28 18:29:12 +0530

    all paths including 4 directions

--- Recursion/Backtracking/AllPath.java
+++ Recursion/Backtracking/AllPath.java
@@ -31,3 +31,43 @@ static void allPath(String p, boolean[][] maze, int r, int c) {
         // so before the function gets removed, also remove the changes that were made by that function
         maze[r][c] = true;
     }
+
+
+//All path printing
+
+
+    static void allPathPrint(String p, boolean[][] maze, int r, int c, int[][] path, int step) {
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
+            path[r][c] = step;
+            for(int[] arr : path) 
+                System.out.println(Arrays.toString(arr));
+            
+            System.out.println(p);
+            System.out.println();
+            return;
+        }
+
+        if (!maze[r][c]) 
+            return;
+        
+
+        // block in path
+        maze[r][c] = false;
+        path[r][c] = step;
+        if (r < maze.length - 1) 
+            allPathPrint(p + 'D', maze, r+1, c, path, step+1);
+
+        if (c < maze[0].length - 1) 
+            allPathPrint(p + 'R', maze, r, c+1, path, step+1);
+
+        if (r > 0) 
+            allPathPrint(p + 'U', maze, r-1, c, path, step+1);
+
+        if (c > 0) 
+            allPathPrint(p + 'L', maze, r, c-1, path, step+1);
+
+        // this line is where the function will be over
+        // so before the function gets removed, also remove the changes that were made by that function
+        maze[r][c] = true;
+        path[r][c] = 0;
+    }
